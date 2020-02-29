0ad5263001263811bdbbabc282bc618b665c2197
alan <alanarostegui@hotmail.com>
2020-02-29 17:56:39 -0600

    404 error: (styles miss)

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Dashboard from '../components/view/Dashboard/Dashboard';
+import NotFound from '../core/404/404NotFound'
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Login from '../components/Auth/Login';
@@ -13,6 +14,7 @@ const Routes = () => (
         <Route exact path="/" component={Dashboard} />
         <Route path="/login" component={Login} />
         <Route path="/register" component={Register} />
+        <Route component={NotFound} />
         </Switch>
     </Router>
 )
