13ed7092edf78ab1e1493ae22054de1f2dae9762
alan <alanarostegui@hotmail.com>
2020-03-24 00:10:10 -0600

    feature(history dependency): implement history in app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Routes from './routes/index';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Router } from "react-router-dom";
+import history from './modules/history/history';
+import Routes from "./routes/index";
 
-ReactDOM.render(<Routes />, document.getElementById('root'));
+ReactDOM.render(
+  <Router history={history}>
+    <Routes />
+  </Router>,
+  document.getElementById("root")
+);
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,28 +1,26 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
 import Dashboard from "../components/view/Dashboard/DashboardContainer";
 import NotFound from "../core/404/404NotFound";
 import Login from "../components/auth/Login";
 import Register from "../components/auth/Register";
-import Settings from '../components/user/Settings';
-import NewAuction from '../components/view/NewAuction/NewAuction'
-import AuctionView from '../components/view/AuctionView/Auction'
+import Settings from "../components/user/Settings";
+import NewAuction from "../components/view/NewAuction/NewAuction";
+import AuctionView from "../components/view/AuctionView/Auction";
 
 import "semantic-ui-css/semantic.min.css";
 
 const Routes = () => (
-  <Router>
-      <Switch>
-        <Route exact path="/" component={Dashboard} />
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
-        <Route path="/user/settings" component={Settings} />
-        <Route path="/create/auction" component={NewAuction} />
-        <Route path="/auctionid" component={AuctionView} />
-        <Route component={NotFound} />
-      </Switch>
-  </Router>
+  <Switch>
+    <Route exact path="/" component={Dashboard} />
+    <Route path="/login" component={Login} />
+    <Route path="/register" component={Register} />
+    <Route path="/user/settings" component={Settings} />
+    <Route path="/create/auction" component={NewAuction} />
+    <Route path="/auction/:id" component={AuctionView} />
+    <Route component={NotFound} />
+  </Switch>
 );
 
 export default Routes;
