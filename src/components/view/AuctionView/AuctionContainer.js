190d1c6bbed65d16f478f91f4cb37f4333960589
alan <alanarostegui@hotmail.com>
2020-04-05 14:15:03 -0500

    feature(auctionContainer): fetch auction

---
+++ src/components/view/AuctionView/AuctionContainer.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+
+import RealTimeAuction from "../RealTimeAuction/RealTimeAuctionView";
+import Auction from './Auction';
+
+import { getAuctionById } from "../../../api";
+
+import 'moment/locale/es';
+
+
+const AuctionContainer = ({match}) => {
+  const [error, setError] = useState(false);
+  const [auction, setAuction] = useState([]);
+
+  const fetchAuction = async () => {
+    const { id } = match.params
+    const response = await getAuctionById(id);
+
+    if (response.status === 200) {
+      setAuction(response.data.body)
+    }
+  }
+
+  useEffect(() => {
+    fetchAuction()
+  }, [])
+
+  console.log('a', auction)
+
+  return (
+    <Auction auction={auction} />
+  );
+};
+
+export default AuctionContainer;
