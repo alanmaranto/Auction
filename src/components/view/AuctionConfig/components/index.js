4858cca52eac43b9a0bbdc646172286aa8479c49
marygch <maria.r.garcia.ch@gmail.com>
2021-01-31 16:58:18 -0600

    feat(fa): rename moveto step
    Signed-off-by: marygch <maria.r.garcia.ch@gmail.com>

--- src/components/view/AuctionConfig/components/index.js
+++ src/components/view/AuctionConfig/components/index.js
@@ -1,6 +1,6 @@
 import { AuctionHeader } from "./header";
 import SuppliersTable from "./invitedSuppliersTable/SuppliersTable";
 import Menu from "./menu/Menu";
-import MoveToStepModal from "./moveToStep/MoveToStep";
+import MoveToStepModal from "./moveToStep/MoveToStepName";
 
 export { AuctionHeader, SuppliersTable, Menu, MoveToStepModal };
