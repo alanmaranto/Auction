b0109815d0a54bbe5322e1f7eedb5d5977491aea
alan <alanarostegui@hotmail.com>
2021-03-06 12:16:47 -0600

    enhancement(socket): add socket connection automatically in app

---
+++ src/hooks/socket/useSocket.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useMemo } from "react";
+import io from "socket.io-client";
+
+export const useSocket = (host) => {
+  const socket = useMemo(
+    () =>
+      io.connect(host, {
+        transports: ["websocket"],
+        autoConnect: true,
+        forceNew: true,
+        query: {
+            'x-token': ''
+        }
+      }),
+    [host]
+  );
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log("conecto");
+    });
+  }, [socket]);
+
+  useEffect(() => {
+    socket.on("disconnect", () => {
+      console.log("desconectó");
+    });
+  }, [socket]);
+
+  return { socket };
+};
