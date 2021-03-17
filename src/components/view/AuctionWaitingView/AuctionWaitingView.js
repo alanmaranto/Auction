2eb428ce06c82d9dfb607c5968982c849ddebeb4
alan <alanarostegui@hotmail.com>
2021-03-17 00:05:52 -0600

    feature(auction waiting view): add auction resume in providers

--- src/components/view/AuctionWaitingView/AuctionWaitingView.js
+++ src/components/view/AuctionWaitingView/AuctionWaitingView.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from "react";
-import { Grid } from "semantic-ui-react";
+import { Grid, Container, Card, Header, Icon } from "semantic-ui-react";
 import history from "../../../modules/history/history";
 import { Row, Column } from "../../../core/indexSemanticUi";
 import { roles } from "../../../helpers/roles";
@@ -39,10 +39,19 @@ const renderer = ({ days, hours, minutes, seconds, completed }) => {
   }
 };
 
-const Auction = ({
-  auction,
-}) => {
-  const { title, description, _id, openingRealTimeAuctionDate } = auction;
+const Auction = ({ auction }) => {
+  const {
+    title,
+    identifier,
+    _id,
+    openingRealTimeAuctionDate,
+    auctionStep,
+    currency,
+    minimumBid,
+    totalItemsPrice,
+    extensionTime,
+    items,
+  } = auction;
   const operation = new Date(openingRealTimeAuctionDate).getTime();
 
   const renderBuyerAuctionView = () => {
@@ -76,7 +85,7 @@ const Auction = ({
         <Fragment>
           <Row>
             <Column>
-              <div style={{ textAlign: "center" }}>
+              <div>
                 <h2>Tiempo para iniciar la subasta</h2>
               </div>
               <Countdown
@@ -87,6 +96,112 @@ const Auction = ({
                 }}
               />
             </Column>
+          </Row>
+          <Row>
+            <Card fluid>
+              <Container>
+                <Row style={{ padding: "15px 15px 10px" }}>
+                  <Header as="h3" color="pink">
+                    <Icon name="info" />
+                    <Header.Content>Información de la subasta</Header.Content>
+                  </Header>
+                </Row>
+                <Row
+                  style={{
+                    justifyContent: "space-between",
+                    padding: "10px 25px",
+                  }}
+                >
+                  <Column
+                    mobile={16}
+                    tablet={16}
+                    computer={4}
+                    largeScreen={4}
+                    widescreen={4}
+                  >
+                    <label className="auction-resume-label">Nombre de la subasta:</label>
+                    <p>{title}</p>
+                  </Column>
+                  <Column
+                    mobile={16}
+                    tablet={16}
+                    computer={4}
+                    largeScreen={4}
+                    widescreen={4}
+                  >
+                    <label className="auction-resume-label">Identificador de la subasta:</label>
+                    <p>{identifier}</p>
+                  </Column>
+                  <Column
+                    mobile={16}
+                    tablet={16}
+                    computer={4}
+                    largeScreen={4}
+                    widescreen={4}
+                  >
+                    <label className="auction-resume-label">Estado de la subasta:</label>
+                    <p>{auctionStep}</p>
+                  </Column>
+                  <Column
+                    mobile={16}
+                    tablet={16}
+                    computer={4}
+                    largeScreen={4}
+                    widescreen={4}
+                  >
+                    <label className="auction-resume-label">Moneda:</label>
+                    <p>{currency}</p>
+                  </Column>
+                </Row>
+                <Row
+                  style={{
+                    justifyContent: "space-between",
+                    padding: "10px 25px",
+                  }}
+                >
+                  <Column
+                    mobile={16}
+                    tablet={16}
+                    computer={4}
+                    largeScreen={4}
+                    widescreen={4}
+                  >
+                    <label className="auction-resume-label">Puja mínima</label>
+                    <p>{minimumBid}</p>
+                  </Column>
+                  <Column
+                    mobile={16}
+                    tablet={16}
+                    computer={4}
+                    largeScreen={4}
+                    widescreen={4}
+                  >
+                    <label className="auction-resume-label">La subasta arranca en:</label>
+                    <p>{totalItemsPrice}</p>
+                  </Column>
+                  <Column
+                    mobile={16}
+                    tablet={16}
+                    computer={4}
+                    largeScreen={4}
+                    widescreen={4}
+                  >
+                    <label className="auction-resume-label">Extensión de:</label>
+                    <p>{extensionTime} minutos</p>
+                  </Column>
+                  <Column
+                    mobile={16}
+                    tablet={16}
+                    computer={4}
+                    largeScreen={4}
+                    widescreen={4}
+                  >
+                    <label className="auction-resume-label">Artículos:</label>
+                    <p>{items?.length}</p>
+                  </Column>
+                </Row>
+              </Container>
+            </Card>
           </Row>
         </Fragment>
       )
